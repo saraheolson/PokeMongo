8cb8bbd8d3cc0876a6baca870179fbd4e54dbaa9
Sarah Olson <solson@trello.com>
2016-10-26 14:22:52 -0500

    Added node names and debugger for physics bodies.

--- PokeMongo/Scenes/GameScene.swift
+++ PokeMongo/Scenes/GameScene.swift
@@ -53,6 +53,8 @@ class GameScene: SKScene {
         
         // Record the beginning of the touch event
         startTouchLocation = touches.first!.location(in: self)
+        
+        checkPhysics()
     }
     
     /**
@@ -109,6 +111,8 @@ class GameScene: SKScene {
         ball = SKSpriteNode(imageNamed: "Ball")
         if let ball = ball {
             
+            ball.name = "Ball"
+            
             // Add physics
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.frame.size.width/2)
             ball.physicsBody?.isDynamic = true
@@ -162,6 +166,8 @@ class GameScene: SKScene {
         monster = SKSpriteNode(texture: spriteArray[0])
         
         if let monster = self.monster {
+            
+            monster.name = "Monster"
             
             monster.position = CGPoint(x: 0, y: frame.height/2)
             monster.zPosition = 2
@@ -224,5 +230,50 @@ extension GameScene: SKPhysicsContactDelegate {
                 }
             }
         }
+    }
+    
+    //MARK: - Analyse the collision/contact set up.
+    func checkPhysics() {
+        
+        // Create an array of all the nodes with physicsBodies
+        var physicsNodes = [SKNode]()
+        
+        //Get all physics bodies
+        enumerateChildNodes(withName: "//.") { node, _ in
+            if let _ = node.physicsBody {
+                physicsNodes.append(node)
+            } else {
+                print("\(node.name) does not have a physics body so cannot collide or be involved in contacts.")
+            }
+        }
+        
+        //For each node, check it's category against every other node's collion and contctTest bit mask
+        for node in physicsNodes {
+            let category = node.physicsBody!.categoryBitMask
+            // Identify the node by its category if the name is blank
+            let name = node.name != nil ? node.name : "Category \(category)"
+            let collisionMask = node.physicsBody!.collisionBitMask
+            let contactMask = node.physicsBody!.contactTestBitMask
+            
+            // If all bits of the collisonmask set, just say it collides with everything.
+            if collisionMask == UInt32.max {
+                print("\(name) collides with everything")
+            }
+            
+            for otherNode in physicsNodes {
+                if (node != otherNode) && (node.physicsBody?.isDynamic == true) {
+                    let otherCategory = otherNode.physicsBody!.categoryBitMask
+                    // Identify the node by its category if the name is blank
+                    let otherName = otherNode.name != nil ? otherNode.name : "Category \(otherCategory)"
+                    
+                    // If the collisonmask and category match, they will collide
+                    if ((collisionMask & otherCategory) != 0) && (collisionMask != UInt32.max) {
+                        print("\(name) collides with \(otherName)")
+                    }
+                    // If the contactMAsk and category match, they will contact
+                    if (contactMask & otherCategory) != 0 {print("\(name) notifies when contacting \(otherName)")}
+                }
+            }
+        }  
     }
 }
